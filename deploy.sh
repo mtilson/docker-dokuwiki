9cb4b48acd6a318eabce4b98ff64740711d7ea29
Ubuntu <m@tilson.biz>
2018-10-16 21:20:09 +0000

    switch to Host + Path

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# docker-compose run -e DEBUG=1 web python console.py
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
+ME="$( basename "${BASH_SOURCE[0]}" )"
+ENV="${DIR}/${ME%\.sh}.env"
+
+test -f "$ENV" || { echo "$ME: error: .env file ($ENV) doesn't exist, exiting" ; exit 255 ; }
+source $ENV
+
+# DW_FE_HOST=
+# DW_FE_PATH=
+# DW_DOMAIN=
+# DW_BACKUP_GIT_REMOTE_URL=
+# DW_PERSISTENT_DIR=
+
+DW_BACKUP_GIT_SERVER_NAME=$(echo $DW_BACKUP_GIT_REMOTE_URL | cut -d"@" -f2 | cut -d":" -f1)
+test -n "$DW_BACKUP_GIT_SERVER_NAME" || 
+    { echo "$ME: error: invalid git remote url ($DW_BACKUP_GIT_REMOTE_URL) defined in .env file ($ENV), exiting" ; exit 255 ; }
+
+test -n "$DW_PERSISTENT_DIR" || 
+    { echo "$ME: error: persistent dir variable (DW_PERSISTENT_DIR) is not defined in .env file ($ENV), exiting" ; exit 255 ; }
+
+echo "$ME: log: recreate $DW_PERSISTENT_DIR"
+sudo rm -fr "$DW_PERSISTENT_DIR"
+sudo mkdir -p "$DW_PERSISTENT_DIR"
+sudo touch "${DW_PERSISTENT_DIR}/acme.json"
+sudo chmod 600 "${DW_PERSISTENT_DIR}/acme.json"
+
+echo "$ME: log: docker-compose pull and up"
+docker-compose pull
+docker-compose up -d
+
+cat < _EOF
+$ME: log: finished succesfully; run the following command to see container logs, use Ctrl-C to exit
+docker-compose logs -f
+_EOF
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-test -n "$GIT_REMOTE_URL" || { echo "[entrypoint.sh] error: GIT_REMOTE_URL is empty"; exit 255; }
-test -n "$GIT_SERVER_NAME" || { echo "[entrypoint.sh] error: GIT_SERVER_NAME is empty"; exit 255; }
+test -n "$DW_BACKUP_GIT_REMOTE_URL" || 
+    { echo "$0: error: variable DW_BACKUP_GIT_REMOTE_URL is not defined, exiting" ; exit 255 ; }
 
-DW_HOST=${DW_HOST:-"dokuwiki"}
+DW_BACKUP_GIT_SERVER_NAME=$(echo $DW_BACKUP_GIT_REMOTE_URL | cut -d"@" -f2 | cut -d":" -f1)
+test -n "$DW_BACKUP_GIT_SERVER_NAME" ||
+    { echo "$0: error: invalid git remote url ($DW_BACKUP_GIT_REMOTE_URL), exiting" ; exit 255 ; }
+
 DW_DOMAIN=${DW_DOMAIN:-"example.com"}
+
 TZ=${TZ:-"UTC"}
 MEMORY_LIMIT=${MEMORY_LIMIT:-"256M"}
 UPLOAD_MAX_SIZE=${UPLOAD_MAX_SIZE:-"16M"}
@@ -15,29 +19,29 @@ function runas_nginx() {
 }
 
 function plugins_and_templates() {
-  echo -n "[entrypoint.sh] bundled plugins saved: /tmp/bundledPlugins.txt, "
+  echo -n "$0: log: bundled plugins saved: /tmp/bundledPlugins.txt, "
   bundledPlugins=$(ls -d /var/www/lib/plugins/*/ | cut -f6 -d'/')
   > /tmp/bundledPlugins.txt
   for bundledPlugin in ${bundledPlugins}; do
     echo "${bundledPlugin}" >> /tmp/bundledPlugins.txt
   done
-  echo " $(wc -l < /tmp/bundledPlugins.txt) found"
+  echo "$0: log: $(wc -l < /tmp/bundledPlugins.txt) found"
 
-  echo -n "[entrypoint.sh] bundled templates saved: /tmp/bundledTpls.txt, "
+  echo -n "$0: log: bundled templates saved: /tmp/bundledTpls.txt, "
   bundledTpls=$(ls -d /var/www/lib/tpl/*/ | cut -f6 -d'/')
   > /tmp/bundledTpls.txt
   for bundledTpl in ${bundledTpls}; do
     echo "${bundledTpl}" >> /tmp/bundledTpls.txt
   done
-  echo " $(wc -l < /tmp/bundledTpls.txt) found"
+  echo "$0: log: $(wc -l < /tmp/bundledTpls.txt) found"
 
   userPlugins=$(ls -l /data/plugins | egrep '^d' | awk '{print $9}')
   for userPlugin in ${userPlugins}; do
     if [ -d /var/www/lib/plugins/${userPlugin} ]; then
-      echo "[entrypoint.sh] WARNING: Plugin ${userPlugin} will not be used (already bundled in DokuWiki)"
+      echo "$0: log: WARNING: Plugin ${userPlugin} will not be used (already bundled in DokuWiki)"
       continue
     fi
-    echo "[entrypoint.sh] link: /var/www/lib/plugins/${userPlugin} -> /data/plugins/${userPlugin}"
+    echo "$0: log: link: /var/www/lib/plugins/${userPlugin} -> /data/plugins/${userPlugin}"
     ln -sf /data/plugins/${userPlugin} /var/www/lib/plugins/${userPlugin}
     chown -h nginx: /var/www/lib/plugins/${userPlugin}
   done
@@ -45,40 +49,40 @@ function plugins_and_templates() {
   userTpls=$(ls -l /data/tpl | egrep '^d' | awk '{print $9}')
   for userTpl in ${userTpls}; do
     if [ -d /var/www/lib/tpl/${userTpl} ]; then
-      echo "[entrypoint.sh] WARNING: Template ${userTpl} will not be used (already bundled in DokuWiki)"
+      echo "$0: log: WARNING: Template ${userTpl} will not be used (already bundled in DokuWiki)"
       continue
     fi
-    echo "[entrypoint.sh] link: /var/www/lib/tpl/${userTpl} -> /data/tpl/${userTpl}"
+    echo "$0: log: link: /var/www/lib/tpl/${userTpl} -> /data/tpl/${userTpl}"
     ln -sf /data/tpl/${userTpl} /var/www/lib/tpl/${userTpl}
     chown -h nginx: /var/www/lib/tpl/${userTpl}
   done
 }
 
-echo "[entrypoint.sh] timezone: ${TZ}"
+echo "$0: log: timezone: ${TZ}"
 ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime
 echo ${TZ} > /etc/timezone
 
 # PHP
-echo "[entrypoint.sh] php-fpm config file: /etc/php7/php-fpm.d/www.conf"
+echo "$0: log: php-fpm config file: /etc/php7/php-fpm.d/www.conf"
 sed -e "s/@MEMORY_LIMIT@/$MEMORY_LIMIT/g" \
   -e "s/@UPLOAD_MAX_SIZE@/$UPLOAD_MAX_SIZE/g" \
   /tpls/etc/php7/php-fpm.d/www.conf > /etc/php7/php-fpm.d/www.conf
 
 # OpCache
-echo "[entrypoint.sh] opcache config file: /etc/php7/conf.d/opcache.ini"
+echo "$0: log: opcache config file: /etc/php7/conf.d/opcache.ini"
 sed -e "s/@OPCACHE_MEM_SIZE@/$OPCACHE_MEM_SIZE/g" \
   /tpls/etc/php7/conf.d/opcache.ini > /etc/php7/conf.d/opcache.ini
 
 # Nginx
-echo "[entrypoint.sh] nginx config file: /etc/nginx/nginx.conf"
+echo "$0: log: nginx config file: /etc/nginx/nginx.conf"
 sed -e "s/@UPLOAD_MAX_SIZE@/$UPLOAD_MAX_SIZE/g" \
   /tpls/etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 
-echo "[entrypoint.sh] php preload file: /var/www/inc/preload.php"
+echo "$0: log: php preload file: /var/www/inc/preload.php"
 cp -f /tpls/preload.php /var/www/inc/
 chown nginx: /var/www/inc/preload.php
 
-echo "[entrypoint.sh] php install file: /var/www/install.php"
+echo "$0: log: php install file: /var/www/install.php"
 install_php_present=0
 test -f /var/www/install.php &&
   { sed -i "1s/.*/<?php define('DOKU_CONF', '\/data\/conf\/'); define('DOKU_LOCAL', '\/data\/conf\/');/" /var/www/install.php; 
@@ -92,7 +96,7 @@ EOF
 message=", (it was not changed this run)"
 test -d ~/.ssh || { mkdir -p ~/.ssh; chmod 700 ~/.ssh; }
 test -f ~/.ssh/id_rsa || {
-  ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "${DW_HOST}@${DW_DOMAIN}";
+  ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -C "dokuwiki-backup@${DW_DOMAIN}";
   message=", (it was newly generated this run)"
   }
 
@@ -102,33 +106,33 @@ do
     if [ -s ~/.ssh/known_hosts ]; then
         break
     else
-        ssh-keyscan $GIT_SERVER_NAME > ~/.ssh/known_hosts
+        ssh-keyscan $DW_BACKUP_GIT_SERVER_NAME > ~/.ssh/known_hosts
         sleep 1
     fi
 done
 
-git config --global user.email "${DW_HOST}@${DW_DOMAIN}"
-git config --global user.name "${DW_HOST}"
+git config --global user.email "dokuwiki-backup@${DW_DOMAIN}"
+git config --global user.name "dokuwiki-backup"
 
-test -f ~/.ssh/id_rsa.pub || { echo "[entrypoint.sh] error: no ~/.ssh/id_rsa.pub file"; exit 255; }
+test -f ~/.ssh/id_rsa.pub || { echo "$0: error: no ~/.ssh/id_rsa.pub file, exiting"; exit 255; }
 
-echo "[entrypoint.sh] $GIT_SERVER_NAME git server public key:"
+echo "$DW_BACKUP_GIT_SERVER_NAME git server public key:"
 cat ~/.ssh/id_rsa.pub
 
 delay=60
-echo "[entrypoint.sh] sleeping for $delay seconds to add the above key to git server account $message"
+echo "sleeping for $delay seconds to add the above key to git server account $message"
 sleep $delay
 
 data_commited=0
 if [ ! -d /data/.git ]; then
-    echo "[entrypoint.sh] clone $GIT_REMOTE_URL to /data"
+    echo "$0: log: clone $DW_BACKUP_GIT_REMOTE_URL to /data"
     rm -fr /data/*
-    git clone $GIT_REMOTE_URL /data
+    git clone $DW_BACKUP_GIT_REMOTE_URL /data
 
-    test -d /data/.git/objects || { echo "[entrypoint.sh] error: no /data/.git/objects after cloning repo"; exit 255; }
+    test -d /data/.git/objects || { echo "$0: error: no /data/.git/objects after cloning repo, exiting"; exit 255; }
     object_count=$(find /data/.git/objects -type f | wc -l)
 
-    test -n "$object_count" || { echo "[entrypoint.sh] error: number of objects is empty"; exit 255; }
+    test -n "$object_count" || { echo "$0: error: number of objects is empty, exiting"; exit 255; }
     if [ "$object_count" -eq "0" ]; then
 
         mkdir -p /data/plugins /data/tpl
@@ -143,7 +147,7 @@ if [ ! -d /data/.git ]; then
 
         chown -R nginx: /data
 
-        echo "[entrypoint.sh] commit /data to $GIT_REMOTE_URL" 
+        echo "$0: log: commit /data to $DW_BACKUP_GIT_REMOTE_URL" 
         cd /data
         git add -A
         git commit -m "wiki created @ `date -u`"
@@ -151,7 +155,7 @@ if [ ! -d /data/.git ]; then
 
         data_commited=1
     else
-        echo "[entrypoint.sh] cloned $GIT_REMOTE_URL to /data"
+        echo "$0: log: cloned $DW_BACKUP_GIT_REMOTE_URL to /data"
 
         test -d /data/data/cache || mkdir /data/data/cache
         test -d /data/data/index || mkdir /data/data/index
@@ -161,14 +165,14 @@ if [ ! -d /data/.git ]; then
         chown -R nginx: /data
     fi
 else
-    echo "[entrypoint.sh] pull $GIT_REMOTE_URL to /data"
+    echo "$0: log: pull $DW_BACKUP_GIT_REMOTE_URL to /data"
     cd /data
     git pull origin master
     chown -R nginx: /data
 fi
 
 if [ $install_php_present -eq "1" -a $data_commited -eq "0" ]; then
-  echo "[entrypoint.sh] /var/www/install.php present and /data was not committed on this run"
+  echo "$0: log: /var/www/install.php present and /data was not committed on this run"
   cp -Rf /var/www/conf /data/
   plugins_and_templates
   chown -R nginx: /data
@@ -181,10 +185,9 @@ fi
 
 if [ -f /data/conf/local.php -o -f /data/conf/users.auth.php -o -f /data/conf/acl.auth.php ]; then
   rm -fr /var/www/install.php
-  echo "[entrypoint.sh] launching '/var/www/bin/indexer.php -c'"
+  echo "$0: log: launching '/var/www/bin/indexer.php -c'"
   runas_nginx 'php7 /var/www/bin/indexer.php -c'
 else
-  echo "[entrypoint.sh]"
   echo ">>"
   echo ">> Point your browser to DokuWiki installation wizard (/install.php) to finish installation."
   echo ">>"
